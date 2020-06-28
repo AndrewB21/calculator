a6cc995b554d8e3ed06bda69212431c6ce7905b6
AndrewB21 <andrewpbumpus@gmail.com>
2020-06-28 21:30:12 +0900

    added styling, decimal functionality

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const buttons = document.querySelectorAll('button');
 const display = document.querySelectorAll('p'); //index 0 = history, index 1 = input
 let inputArray = [];
 let historyArray = [];
-
+let decimalActive = false;
+operatorActive = false;
 
 const updateDisplay =(location, updateText) => {
     if (location.firstChild){
@@ -12,13 +13,38 @@ const updateDisplay =(location, updateText) => {
     location.appendChild(document.createTextNode(updateText))
 }
 
+const appendInput = (input) => {
+    inputArray.push(input);
+    historyArray.push(input);
+}
+
+const evaluate = () => {
+    total = eval(inputArray.join(''));
+    updateDisplay(display[0], historyArray.join(''));
+    updateDisplay(display[1], total);
+    inputArray = [total];
+}
+
 buttons.forEach(button => {
     button.addEventListener('click', function(){
         switch (button.value){
             case '+':
             case '-':
             case '*':
-            case '/': 
+            case '/':
+                 if(operatorActive){
+                    evaluate();
+                    appendInput(button.value);
+                    updateDisplay(display[1], inputArray.join(''));
+                 }
+                 else{
+                    appendInput(button.value);
+                    updateDisplay(display[1], inputArray.join(''));
+                    operatorActive = true;
+                 }
+                 decimalActive = false;
+                 break;
+            case "0":
             case "1":
             case "2":
             case "3":
@@ -28,23 +54,30 @@ buttons.forEach(button => {
             case "7":
             case "8":
             case "9":
-                {
-                inputArray.push(button.value);
-                historyArray.push(button.value);
+                appendInput(button.value);
                 updateDisplay(display[1], inputArray.join(''));
                 break;
-            }
+            case ".":
+                if(decimalActive){
+                    break;
+                }
+                else{
+                    appendInput(button.value);
+                    updateDisplay(display[1], inputArray.join(''));
+                    decimalActive = true;
+                    break;
+                }
             case '=':
-                total = eval(inputArray.join(''));
-                updateDisplay(display[0], historyArray.join(''));
-                updateDisplay(display[1], total);
-                inputArray = [total];
+                evaluate();
                 break;
             case 'C':
                 inputArray = []
                 historyArray = []
                 display[0].innerText = ""
                 display[1].innerText = ""
+                decimalActive = false;
+                operatorActive = false;
+                break;
         }
     })
 })
